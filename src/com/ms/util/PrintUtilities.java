234f0601825834c4dd287784ced86119c97dfd97
Venugopal M <venumanchala.mca@gmail.com>
2014-12-17 19:00:58 +0530

    First Commitment

--- src/com/ms/util/PrintUtilities.java
+++ src/com/ms/util/PrintUtilities.java
@@ -19,81 +19,83 @@ import javax.swing.RepaintManager;
  *
  * @author TRANSDYN\ery
  */
-    public class PrintUtilities implements Printable {  
-      private Component componentToBePrinted;  
-       
-      public static void printComponent(Component c) {  
-        new PrintUtilities(c).print();  
-      }  
-        
-      public PrintUtilities(Component componentToBePrinted) {  
-        this.componentToBePrinted = componentToBePrinted;  
-      }  
-        
-      public void print() {  
-        PrinterJob printJob = PrinterJob.getPrinterJob();  
-        printJob.setPrintable(this);  
-        if (printJob.printDialog())  
-          try {  
-            printJob.print();
-            printJob.setJobName("hello");
-          } catch(PrinterException pe) {  
-            System.out.println("Error printing: " + pe);  
-          }  
-      }  
-       
-      public int print(Graphics g, PageFormat pageFormat, int pageIndex) {  
-      
-        if (pageIndex > 0) {  
-          return(NO_SUCH_PAGE);  
-        } else {  
-          Graphics2D g2d = (Graphics2D)g;  
-          g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY()-100);  
-          disableDoubleBuffering(componentToBePrinted);  
-    // scale to fill the page          
-    double dw = pageFormat.getImageableWidth()-180;  
-    double dh = pageFormat.getImageableHeight();  
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();  
-      
-    double xScale = dw / screenSize.width;  
-    double yScale = dh / screenSize.height;  
-    double scale = Math.min(xScale,yScale);  
-       
-    // center the chart on the page  
-    double tx = 0.0;  
-    double ty = 0.0;  
-    if (xScale > scale)  
-    {  
-        tx = Math.floor(0.5*(xScale-scale)*screenSize.width);  
-    }  
-    else  
-           {  
-        ty = Math.floor(0.5*(yScale-scale)*screenSize.height);  
-    }  
-    g2d.translate(tx, ty);  
-    g2d.scale(scale, scale+0.4);  
-      
-    componentToBePrinted.paint(g2d);  
-    enableDoubleBuffering(componentToBePrinted);  
-    return Printable.PAGE_EXISTS;  
-        }  
-      }  
-       
-      /** The speed and quality of printing suffers dramatically if 
-       *  any of the containers have double buffering turned on. 
-       *  So this turns if off globally. 
-       *  @see enableDoubleBuffering 
-       */  
-      public static void disableDoubleBuffering(Component c) {  
-        RepaintManager currentManager = RepaintManager.currentManager(c);  
-        currentManager.setDoubleBufferingEnabled(false);  
-      }  
-       
-      /** Re-enables double buffering globally. */  
-        
-      public static void enableDoubleBuffering(Component c) {  
-        RepaintManager currentManager = RepaintManager.currentManager(c);  
-        currentManager.setDoubleBufferingEnabled(true);  
-        
-      }  
-    }  
+public class PrintUtilities implements Printable {
+
+    private Component componentToBePrinted;
+
+    public static void printComponent(Component c) {
+        new PrintUtilities(c).print();
+    }
+
+    public PrintUtilities(Component componentToBePrinted) {
+        this.componentToBePrinted = componentToBePrinted;
+    }
+
+    public void print() {
+        PrinterJob printJob = PrinterJob.getPrinterJob();
+        printJob.setPrintable(this);
+        if (printJob.printDialog()) {
+            try {
+                printJob.print();
+                printJob.setJobName("hello");
+            } catch (PrinterException pe) {
+                System.out.println("Error printing: " + pe);
+            }
+        }
+    }
+
+    public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
+
+        if (pageIndex > 0) {
+            return (NO_SUCH_PAGE);
+        } else {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY() - 220);
+            disableDoubleBuffering(componentToBePrinted);
+            // scale to fill the page          
+            double dw = pageFormat.getImageableWidth() - 180;
+            double dh = pageFormat.getImageableHeight();
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+            double xScale = dw / screenSize.width;
+            double yScale = dh / screenSize.height;
+            double scale = Math.min(xScale, yScale);
+
+            // center the chart on the page  
+            double tx = 0.0;
+            double ty = 0.0;
+            if (xScale > scale) {
+                tx = Math.floor(0.5 * (xScale - scale) * screenSize.width);
+            } else {
+                ty = Math.floor(0.5 * (yScale - scale) * screenSize.height);
+            }
+            g2d.translate(tx, ty);
+            g2d.scale(scale, scale + 0.4);
+
+            componentToBePrinted.paint(g2d);
+            enableDoubleBuffering(componentToBePrinted);
+            return Printable.PAGE_EXISTS;
+        }
+    }
+
+    /**
+     * The speed and quality of printing suffers dramatically if any of the
+     * containers have double buffering turned on. So this turns if off
+     * globally.
+     *
+     * @see enableDoubleBuffering
+     */
+    public static void disableDoubleBuffering(Component c) {
+        RepaintManager currentManager = RepaintManager.currentManager(c);
+        currentManager.setDoubleBufferingEnabled(false);
+    }
+
+    /**
+     * Re-enables double buffering globally.
+     */
+    public static void enableDoubleBuffering(Component c) {
+        RepaintManager currentManager = RepaintManager.currentManager(c);
+        currentManager.setDoubleBufferingEnabled(true);
+
+    }
+}
